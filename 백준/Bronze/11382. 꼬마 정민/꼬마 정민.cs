011222f47baca2d3093198757f3310045d77332f
JunseoHo <112461939+JunseoHo@users.noreply.github.com>
2023-08-30 00:40:51 +0900

    [Bronze V] Title: 꼬마 정민, Time: 60 ms, Memory: 5496 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace AlgorithmTraining
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            long A = long.Parse(input[0]);
+            long B = long.Parse(input[1]);
+            long C = long.Parse(input[2]);
+            Console.WriteLine(A + B + C);
+        }
+    }
+}
