66d1a24bb781c4c275d1a0cf5fe8dfcfe35bc004
JunseoHo <112461939+JunseoHo@users.noreply.github.com>
2023-08-30 00:29:01 +0900

    [Bronze V] Title: ??!, Time: 64 ms, Memory: 5372 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace AlgorithmTraining
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Console.ReadLine() + "??!");
+        }
+    }
+}
