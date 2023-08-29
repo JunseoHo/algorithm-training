ac1e82f17006b7314a07aa9a4152abf358525ca0
JunseoHo <112461939+JunseoHo@users.noreply.github.com>
2023-08-30 00:31:56 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 64 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace AlgorithmTraining
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(int.Parse(Console.ReadLine()) - (2541 - 1998));
+        }
+    }
+}
