8976dd14f1954af5782a08dc7191af4eae55d98a
JunseoHo <112461939+JunseoHo@users.noreply.github.com>
2023-08-31 14:47:15 +0900

    [Bronze V] Title: A+B - 8, Time: 64 ms, Memory: 6076 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Text;
+
+namespace AlgorithmTraining
+{
+    class Program
+    {
+        public static void Main(string[] argv)
+        {
+            StringBuilder answer = new StringBuilder();
+            int t = int.Parse(Console.ReadLine());
+            int[] a = new int[t];
+            int[] b = new int[t];
+            for (int i = 0; i < t; i++)
+            {
+                string[] s = Console.ReadLine().Split();
+                a[i] = int.Parse(s[0]);
+                b[i] = int.Parse(s[1]);
+                answer.AppendLine("Case #" + (i + 1) + ": " + a[i] + " + " +b[i] + " = " + (a[i] + b[i]));
+            }
+
+            Console.WriteLine(answer.ToString());
+        }
+    }
+}
