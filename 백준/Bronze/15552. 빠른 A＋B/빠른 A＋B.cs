a25d7aaeb30ce8a020c1df4b9f2d4562560650bc
JunseoHo <112461939+JunseoHo@users.noreply.github.com>
2023-08-31 14:44:53 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1220 ms, Memory: 44864 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Text;
+
+namespace AlgorithmTraining
+{
+    class Program
+    {
+        public static void Main(string[] argv)
+        {
+            StringBuilder answer = new StringBuilder();
+            int t = int.Parse(Console.ReadLine());
+            int[] a = new int[t];
+            int[] b = new int[t];
+            for (int i = 0; i < t; i++)
+            {
+                string[] s = Console.ReadLine().Split();
+                a[i] = int.Parse(s[0]);
+                b[i] = int.Parse(s[1]);
+                answer.AppendLine((a[i] + b[i]).ToString());
+            }
+            Console.WriteLine(answer.ToString());
+        }
+    }
+}
